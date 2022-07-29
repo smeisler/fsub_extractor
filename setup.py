9a93431793b0a86eaa1aec344a192e4b895944e1
Steven Meisler <smeisler@g.harvard.edu>
2022-07-29 12:59:01 -0400

    Add files in anticipation of PyPI release

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = “fsub-extractor”,
+    packages = [“fsub-extractor”],
+    version = “0.0.1”,
+    description = “Software that functionally segments white matter connections to generate task-specific subcomponents of fiber bundles.”,
+    author = “Steven Meisler”,
+    author_email = “smeisler@g.harvard.edu”,
+    url = “https://github.com/smeisler/fsub-extractor”,
+    download_url = “download link you saved (ADD THIS LATER)”,
+    keywords = [“dwi”, “fmri”, “white-matter-segmentation”],
+    classifiers = [],
+)
