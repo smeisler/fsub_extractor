5fdb6aa5c43683311d99b55d097e8a4eff8ea98e
Steven Meisler <smeisler@g.harvard.edu>
2022-08-03 13:43:58 -0400

    Add basic tract extractor and ROI merging function

--- fsub-extractor/main.py
+++ fsub-extractor/main.py
@@ -1,5 +1,8 @@
 import argparse
 import os.path as op
+import os
+import shutil
+import subprocess
 
 def find_program(program):
 
@@ -43,6 +46,10 @@ def get_parser():
         "--scalar",
         help="Scalar map(s) to sample streamlines on (.nii.gz)",
         default=None)
+    parser.add_argument(
+        "--search_dist", "--search-dist"
+        help="Distance in mm to search ahead of streamlines for ROIs",
+        default=4)
     return parser
 
 def main():
@@ -54,20 +61,90 @@ def main():
                         roi2=args.roi2,
                         out_dir=args.out_dir,
                         out_prefix=args.out_prefix,
-			scalar=args.scalar)
-   
+			scalar=args.scalar,
+			search_dist=args.search_dist)
+
+def merge_rois(roi1, roi2, out_file):
+    """Creates the input ROI file to be passed into tck2connectome.
+    If a single ROI file is passed:
+        Copies the same single ROI file.
+    If multiple ROI files are passed:
+        Multiplies the second ROI file passed by 2, and merges this file with the first file. 
+        Returns the merged file
+    Parameters
+    ==========
+    roi1: str
+        abspath to a roi mask file
+    roi2: str [Optional]
+        abspath to a second roi mask file
+ 
+    Outputs
+    =======
+    roi_merge: str
+        abspath to the 
+    """
+    if roi2 == None:
+        shutil.copyfile(roi1, out_file)
+        err_mult = None
+        err_merg = None
+
+    else:
+        labelled_roi2 = roi2.remove_suffix(".nii.gz") + "_labelled.nii.gz"
+        mrcalc_path = find_program("mrcalc")
+        mult_proc = subprocess.Popen([mrcalc_path, roi2, 2, "-mult", labelled_roi2], stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        _, err_mult = mult_proc.communicate()
+        if not op.exists(labelled_roi2):
+            raise Exception(err_mult)
+
+        merg_proc = subprocess.Popen([mrcalc_path, roi1, labelled_roi2, out_file, "-add"], stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
+        _, err_merg = merg_proc.communicate()
+        if not op.exists(out_file):
+            raise Exception(err_mult)
+            
+    return out_file, err_mult, err_merg
+
+def extract_tck_mrtrix(tck_file, rois_in, outpath_base, search_dist): #nodes?
+    # [TODO] docs
+    # Run MRtrix CLI
+    ### tck2connectome
+    tck2connectome_path = find_program('tck2connectome')
+    tck2conn_proc = subprocess.Popen([tck2connectome_path, tck_file, rois_in, outpath_base + "connectome.txt", 
+	'-assignment_forward_search', search_dist, "-out_assignments", outpath_base + "assignments.txt"],
+	stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _, err_tck2conn = tck2conn_proc.communicate()
+    # [TODO] check for output, if not present, print error
+
+    ### connectome2tck
+    connectome2tck_path = find_program('connectome2tck')
+    # [TODO] FIX NODES BASED ON 1 OR 2 ROI INPUTS
+    conn2tck_proc = subprocess.Popen([connectome2tck_path, tck_file, outpath_base + "assignments.txt", outpath_base + "extracted.tck", 
+	'-nodes', "1,2", "-exclusive", "-files single"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    _, err_conn2tck = conn2tck_proc.communicate()
+    # [TODO] check for output, if not present, print error
+
+    return [err_tck2conn, err_conn2tck]
+
 def extractor(tck_file, roi1, roi2, out_dir, out_prefix, scalar):
-    # [TODO] add docus
+    # [TODO] add docs
 
     # Check for assertion errors [TODO]
+
+    outpath_base = op.join(out_dir, out_prefix) + "_"
 
     # Create atlas-like file if multiple ROIs avaialble
     if roi2 == None:
 	rois_in = roi1
     else:
-	rois_in = merge_rois(roi1,roi2)
+	roi1_basename = op.basename(roi1).remove_suffix(".nii.gz")
+	roi2_basename = op.basename(roi2).remove_suffix(".nii.gz")
+	[rois_in, err1, err2] = merge_rois(roi1, roi2, outpath_base + roi1_basename + "_" + roi2_basename + "_merged.nii.gz") # [TODO] make naming more flexible
     # [TODO] check validity of ROI file
 
-    # Run MRtrix CLI
-    command_tck2connectome = "tck2connectome 
+    # Run MRtrix commands
+    cmd_errs = extract_tck_mrtrix(tck_file, rois_in, outpath_base, search_dist)
+
     
+
+
