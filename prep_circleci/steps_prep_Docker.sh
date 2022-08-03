e1f023c2171b4d8bf50c25e5a077f01a3f81210d
zhao-cy <chenyzh@seas.upenn.edu>
2022-08-03 19:40:11 -0400

    add dockerfiles for building docker image for circleci tests

---
+++ prep_circleci/steps_prep_Docker.sh
@@ -0,0 +1,19 @@
+# Steps of preparing the Docker images:
+
+# +++++++++++++++++++++++++++++++++++++++
+tag_version="0.0.0.9000"
+# +++++++++++++++++++++++++++++++++++++++
+
+## build the dockerfile for miniconda
+cmd="docker build -t chenyingzhao/fsub_extractor_miniconda:22.4.1 -f Dockerfile_Miniconda ."
+echo $cmd
+#$cmd
+
+cmd="docker push chenyingzhao/fsub_extractor_miniconda:22.4.1"
+echo $cmd
+#$cmd
+
+## build the dockerfile for fsub_extractor:
+cmd="docker build -t chenyingzhao/fsub_extractor_deps:${tag_version} ."
+echo $cmd
+$cmd
