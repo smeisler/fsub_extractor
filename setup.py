f4e9f17ae6f3b3e9239ca0ef86ad9cfc5cc98776
Steven Meisler <smeisler@g.harvard.edu>
2022-08-03 11:04:54 -0700

    Fix unicode errors in quotation marks

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name = “fsub-extractor”,
-    packages = [“fsub-extractor”],
-    version = “0.0.1”,
-    description = “Software that functionally segments white matter connections to generate task-specific subcomponents of fiber bundles.”,
+    name = "fsub-extractor",
+    packages = ["fsub-extractor"],
+    version = "0.0.1",
+    description = "Software that functionally segments white matter connections to generate task-specific subcomponents of fiber bundles.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author = “Steven Meisler”,
-    author_email = “smeisler@g.harvard.edu”,
-    url = “https://github.com/smeisler/fsub-extractor”,
-    download_url = “download link you saved (ADD THIS LATER)”,
-    keywords = [“dwi”, “fmri”, “white-matter-segmentation”],
+    author = "Steven Meisler",
+    author_email = "smeisler@g.harvard.edu”,
+    url = "https://github.com/smeisler/fsub-extractor",
+    download_url = "download link you saved (ADD THIS LATER)",
+    keywords = ["dwi", "fmri", "white-matter-segmentation"],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
