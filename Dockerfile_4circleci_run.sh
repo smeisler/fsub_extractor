90fbca610b8d242b120b3b0be367a37faa4f64ef
Chenying Zhao <chenyzh@seas.upenn.edu>
2022-08-05 14:53:32 -0400

    add freesurfer to Dockerfile

--- Dockerfile_4circleci_run.sh
+++ Dockerfile_4circleci_run.sh
@@ -1,15 +1,15 @@
 # Steps of preparing the Docker images:
 
 # +++++++++++++++++++++++++++++++++++++++
-tag_version="0.0.1"
+tag_version="0.0.1.9000"
 # +++++++++++++++++++++++++++++++++++++++
 
 
 ## build the dockerfile for fsub_extractor:
 cmd="docker build -t chenyingzhao/fsub_extractor_deps:${tag_version} -f Dockerfile_4circleci ."
 echo $cmd
-#$cmd
+$cmd
 
 cmd="docker push chenyingzhao/fsub_extractor_deps:${tag_version}"
-echo $cmd
-$cmd
+#echo $cmd
+#$cmd
