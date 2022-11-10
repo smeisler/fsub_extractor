bcdd31084392b34f77ca6e72150fbfebcf088798
Steven Meisler <smeisler@g.harvard.edu>
2022-11-10 14:28:41 -0500

    Comment out unneccessary variables, add overwrite checks

--- fsub_extractor/streamline_scalar/streamline_scalar.py
+++ fsub_extractor/streamline_scalar/streamline_scalar.py
@@ -39,23 +39,23 @@ def get_parser():
     parser.add_argument(
         "--scalar_names",
         "--scalar-names",
-        help="Comma delimited list (no spaces) of names to scalar maps (e.g. FA). The number of names must match the number of scalar paths",
+        help="Comma delimited list (no spaces) of names to scalar maps (e.g. Fractional_Anisotropy). The number of names must match the number of scalar paths",
         required=True,
     )
-    parser.add_argument(
-        "--roi_begin",
-        "--roi-begin",
-        help="Binary ROI that will be used to denote where streamlines begin (lower number nodes on tract profiles)",
-        type=op.abspath,
+    #parser.add_argument(
+    #    "--roi_begin",
+    #    "--roi-begin",
+    #    help="Binary ROI that will be used to denote where streamlines begin (lower number nodes on tract profiles)",
+    #    type=op.abspath,
         # required=True,
-    )
-    parser.add_argument(
-        "--roi_end",
-        "--roi-end",
-        help="Binary ROI that will be used to denote where streamlines end (higher number nodes on tract profiles)",
-        type=op.abspath,
+    #)
+    #parser.add_argument(
+    #    "--roi_end",
+    #    "--roi-end",
+    #    help="Binary ROI that will be used to denote where streamlines end (higher number nodes on tract profiles)",
+    #    type=op.abspath,
         # required=True,
-    )
+    #)
     parser.add_argument(
         "--n_points",
         "--n-points",
@@ -77,13 +77,13 @@ def get_parser():
         type=str,
         default="",
     )
-    parser.add_argument(
-        "--scratch",
-        "--scratch",
-        help="Path to scratch directory. Default is current directory.",
-        type=op.abspath,
-        default=os.getcwd(),
-    )
+    #parser.add_argument(
+    #    "--scratch",
+    #    "--scratch",
+    #    help="Path to scratch directory. Default is current directory.",
+    #    type=op.abspath,
+    #    default=os.getcwd(),
+    #)
     parser.add_argument(
         "--overwrite",
         help="Whether to overwrite outputs. Default is to overwrite.",
@@ -103,14 +103,14 @@ def main():
     main = streamline_scalar(
         subject=args.subject,
         tract=args.tract,
-        roi_begin=args.roi_begin,
-        roi_end=args.roi_end,
+        #roi_begin=args.roi_begin,
+        #roi_end=args.roi_end,
         scalar_paths=args.scalar_paths,
         scalar_names=args.scalar_names,
         n_points=args.n_points,
         out_dir=args.out_dir,
         out_prefix=args.out_prefix,
-        scratch=args.scratch,
+        #scratch=args.scratch,
         overwrite=args.overwrite,
     )
 
@@ -118,14 +118,14 @@ def main():
 def streamline_scalar(
     subject,
     tract,
-    roi_begin,
-    roi_end,
+    #roi_begin,
+    #roi_end,
     scalar_paths,
     scalar_names,
     n_points,
     out_dir,
     out_prefix,
-    scratch,
+    #scratch,
     overwrite,
 ):
 
@@ -166,8 +166,8 @@ def streamline_scalar(
     # Check if out and scratch directories exist
     if op.isdir(out_dir) == False:
         raise Exception(f"Output directory {out_dir} not found on the system.")
-    if op.isdir(scratch) == False:
-        raise Exception(f"Scratch directory {scratch} not found on the system.")
+    #if op.isdir(scratch) == False:
+    #    raise Exception(f"Scratch directory {scratch} not found on the system.")
 
     ### Prepare output directories ###
     # Add an underscore to separate prefix from file names if a prefix is specified
@@ -178,14 +178,15 @@ def streamline_scalar(
     # Make subject output and scratch folders if they do not exist, and define the naming convention
     if op.isdir(op.join(out_dir, subject)) == False:
         os.mkdir(op.join(out_dir, subject))
-    if op.isdir(op.join(scratch, subject + "_scratch")) == False:
-        os.mkdir(op.join(scratch, subject + "_scratch"))
+    #if op.isdir(op.join(scratch, subject + "_scratch")) == False:
+    #    os.mkdir(op.join(scratch, subject + "_scratch"))
     subject_base = op.join(out_dir, subject)
     outpath_base = op.join(subject_base, out_prefix)
-    scratch_base = op.join(scratch, subject + "_scratch", out_prefix)
+    #scratch_base = op.join(scratch, subject + "_scratch", out_prefix)
 
     ### Reorient streamlines so beginning of each streamline are at the same end
     tract_loaded = load_tractogram(tract, trk_ref).streamlines
+    # TODO: See if we need to reorient streamlines, and how
     # trk_ref_img, ref_affine = load_nifti(trk_ref)
     # roi_begin_img = load_nifti_data(roi_begin)
     # roi_end_img = load_nifti_data(roi_end)
@@ -219,6 +220,8 @@ def streamline_scalar(
         plt.ylabel(scalar_name)
         plt.xlabel("Node along Bundle")
         profile_fig_outfile = op.join(subject_base, scalar_name + "_profile.png")
+        if overwrite == False:
+            overwrite_check(profile_fig_outfile)
         plt.savefig(profile_fig_outfile)
         # TODO: SAVE OUT CSV AND PLOT
 
@@ -254,6 +257,8 @@ def streamline_scalar(
         n_streamlines = len(streamline_avgs_num)
         # Write summary stats to outfile
         stats_outfile = op.join(subject_base, scalar_name + "_stats.txt")
+        if overwrite == False:
+            overwrite_check(stats_outfile)
         stats_outfile_object = open(stats_outfile, "w")
         stats_string = f"Mean: {tract_avg} \nMedian: {tract_med} \nStandard Deviation: {tract_std} \nTract Profile: {profile_bundle} \nNumber of Streamlines: {n_streamlines}"
         stats_outfile_object.write(stats_string)
