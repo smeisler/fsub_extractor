b4be4d7ece93eab9a7aef268fa3fd5a6a9e4b696
Emily Kubota <emily.kubota@gmail.com>
2023-06-07 10:30:08 -0700

    added dice coefficient

--- fsub_extractor/functions/dice_coefficient.py
+++ fsub_extractor/functions/dice_coefficient.py
@@ -2,13 +2,14 @@ import nibabel as nib
 import subprocess 
 import csv 
 
-def calculate_dice_coefficient(
-	tck_file1,
-	tck_file2,
-	ref_anat,
-	out_prefix,
-	out_path):
- """Uses tckmap to transform two input tck_files to nifti images and then calculate the dice overlap.
+def dice_coefficient(
+    tck_file1,
+    tck_file2, 
+    ref_anat,
+    out_prefix,
+    out_path
+):
+    """Uses tckmap to transform two input tck_files to nifti images and then calculate the dice overlap.
 
     Parameters
     ==========
@@ -29,30 +30,32 @@ def calculate_dice_coefficient(
   
     """
 
-   # Transform both tck files to nifti using tckmap
-   out_name1=tck_file1[-3]+'nii.gz'
-   subprocess.run('tckmap -template '+ref_anat+' '+tck_file1+' '+out_name1) 
+    # Transform both tck files to nifti using tckmap
+    out_name1=tck_file1[:-3]+'nii.gz'
+    subprocess.run("tckmap -template "+ref_anat+" "+tck_file1+" "+out_name1,shell=True) 
 
-   out_name2=tck_file2[-3]+'nii.gz'
-   subprocess.run('tckmap -template '+ref_anat+' '+tck_file2+' '+out_name2)
+    out_name2=tck_file2[:-3]+'nii.gz'
+    subprocess.run("tckmap -template "+ref_anat+" "+tck_file2+" "+out_name2,shell=True)
 
 
-   # Read in images
-   img1 = nib.load(out_name1)
-   img2 = nib.load(out_name2)
+    # Read in images
+    img1 = nib.load(out_name1)
+    img2 = nib.load(out_name2)
   
-   data1 = img1.get_fdata()
-   data2 = img2.get_fdata()
+    data1 = img1.get_fdata()
+    data2 = img2.get_fdata()
 
-   overlap = sum(sum(sum((data1 > 0) & (data2 > 0))))
-   track1 = sum(sum(sum(data1 > 0)))
-   track2 = sum(sum(sum(data2 > 0)))
+    overlap = sum(sum(sum((data1 > 0) & (data2 > 0))))
+    sum1 = sum(sum(sum(data1 > 0)))
+    sum2 = sum(sum(sum(data2 > 0)))
 
-   dice = 2*overlap/(sum1+sum2)
+    dice = 2*overlap/(sum1+sum2)
 
-   f = open(out_path+'/'+out_prefix+'_dice.csv','w')
-   writer = csv.writer(f)
-   writer.writerow(dice)
-   f.close()
+    f = open(out_path+'/'+out_prefix+'_dice.csv','w')
+    writer = csv.writer(f)
+    writer.writerow([dice])
+    f.close()
+ 
+
 
    
